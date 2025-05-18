869b5a71c24cfd952f01262f5f43c19671ffe8bc
Ioana-Călina Pascu <94381283+ultracln@users.noreply.github.com>
2025-05-18 19:06:54 +0300

    changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,8 @@ docker stack deploy -c portainer-stack.yml portainer
 
 echo "Deploying Monitoring stack..."
 docker stack deploy -c prometheus-stack.yml monitoring
+
+echo "Forcing service updates to ensure latest images are used..."
+docker service update --image ultracln/auth-service:latest bookswap_auth-service
+docker service update --image ultracln/book-service:latest bookswap_book-service
+docker service update --image ultracln/frontend:latest bookswap_frontend
